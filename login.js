ef8075420f8791004c65c90148c6030b8adc6668
Carlos Bello <cjbh84@gmail.com>
2020-03-20 17:07:38 -0400

    removed old files, starting fresh

--- login.js
+++
@@ -1,9 +0,0 @@
-function check(txt) {
-    if (username.value != '' &&
-        pass.value.length >= 1) {
-        document.getElementById("submit").disabled  = false;
-    }
-    else {
-        document.getElementById("submit").disabled = true;
-    }
-}
