c4440a89c1c1e8cc9789ade6ce4514173d8c45a6
Carlos Bello <cjbh84@gmail.com>
2020-04-15 05:35:39 -0400

    Fix functions.js
    Added fadeIn() and fadeOut()
    Integrated fadeIn() and fadeOut() into refactored loadFragment()

--- forms/forms.js
+++ forms/forms.js
@@ -1,20 +1,7 @@
 window.onload = function() {
-    loadFragment("forms/login.html");
+    loadFragment("forms/login.html", document.getElementById("form", 1));
 }
 
-function loadFragment(url) {
-    //Create request and select form div
-    var xhr = new XMLHttpRequest();
-    var form = document.getElementById('form');
-    //Set request, send it
-    xhr.open("GET", url, true);
-    xhr.send();
-    //Handle request
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            form.innerHTML = xhr.responseText;
-        }
-    }
-}
+
 
 
--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,67 @@
-function fadeIn(el) {
-    el.style.opacity = 0;
+//------------------------------------------------Animation
+//We probably won't call fadeIn() or fadeOut() directly,
+//they are really just helper functions for loadFragment()
+function fadeIn (element)  {
+        element.style.display = 'block';
+        let opacity = 0;
+        let request;
 
-    var last = +new Date();
-    var tick = function() {
-        el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
-        last = +new Date();
+        const animation = () => {
+            element.style.opacity = opacity += 0.01;
+            if (opacity == 1) {
+                cancelAnimationFrame(request);
+            }
+        };
 
-        if (+el.style.opacity < 1) {
-            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+        const rAf = () => {
+            request = requestAnimationFrame(rAf);
+            animation();
+        };
+        rAf();
+};
+
+function fadeOut(element) {
+    let opacity = 1;
+    let request;
+
+    const animation = () => {
+        element.style.opacity = opacity -= 0.01;
+        if (opacity == 0) {
+            cancelAnimationFrame(request);
         }
     };
 
-    tick();
+    const rAf = () => {
+        request = requestAnimationFrame(rAf);
+        animation();
+    };
+    rAf();
+    element.style.display = 'none';
 }
-window.onload = function () {
-    var el = document.getElementById('form');
-    fadeIn(el);
+
+//-----------------------------------------------------AJAX
+//For most cases, you will call this function without
+//a value for fade. The only time you won't, you'll use a 0
+//with. Do this when loding a page for the first time while
+//the element is hidden by "display: none" in the css
+function loadFragment(url, element, fade = 1) {
+    //Create request and select form div
+    var xhr = new XMLHttpRequest();
+    //Set request, send it
+    xhr.open("GET", url, true);
+    xhr.send();
+    //Handle request
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            if (fade == 0) {
+                element.innerHTML = xhr.responseText;
+                fadeIn(element);
+            }
+            else {
+                fadeOut(element);
+                element.innerHTML = xhr.responseText;
+                fadeIn(element);
+            }
+        }
+    }
 }
