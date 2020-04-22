8c980122920704d474ec233599f1a2fe69348973
Carlos Bello <cjbh84@gmail.com>
2020-04-22 04:04:29 -0400

    Add php scripts that create and populate tables
    New files include:
    
    php/useradder.php - requires users.php, config.php
    php/users.php

---
+++ php/users.php
@@ -0,0 +1,26 @@
+<?php
+$names = [
+	1  => "John",
+	2  => "Mary",
+	3  => "Michael",
+	4  => "Susan",
+	5  => "Chris",
+	6  => "Karen",
+	7  => "Brian",
+	8  => "Amanda",
+	9  => "Edward",
+	10 => "Laura"
+];
+$usernames = [
+	1  => "nhon",
+	2  => "yram",
+	3  => "leahcim",
+	4  => "nasus",
+	5  => "sirhc",
+	6  => "nerak",
+	7  => "nairb",
+	8  => "adnama",
+	9  => "drawde",
+	10 => "arual"
+];
+?>
