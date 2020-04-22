fb87ab6059718eac1b438cc80c9c365a623b985a
Adrian Cuvi <adrian.cuvi@stu.bmcc.cuny.edu>
2020-04-22 02:29:47 -0400

    update register forms

---
+++ forms/register.php
@@ -0,0 +1,35 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$email = filter_input(INPUT_POST, 'email');
+$group = filter_input(INPUT_POST, 'groupNumber');
+$password = filter_input(INPUT_POST, 'password');
+$password2 = filter_input(INPUT_POST, 'password2');
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "Vladimir99";
+$dbname = "StatTracker";
+
+//creating a connection with  database
+//mysqli_connect_errno() Returns the error code from last connect call
+//mysqli_connect_error() Returns a string description of the last connect error
+$connection = new mysqli ($host, $dbusername, $dbpasswoord, $dbname);
+if(mysqli_connect_error()){
+  die('Error Connecting ('.mysqli_connect_errno().')'
+    .mysqli_connect_error());
+}
+
+else {
+    if($password == $password2){
+        $sql = "INSERT INTO Users(username, email, group, password)
+        values ('$username', '$email', '$group', '$password')";
+        if(connection->query($sql)){
+            echo "New data has been inserted succesfully";
+        }
+    else {
+    echo "Some error has occurred".$sql."<br>".$connection->error;
+    }
+        $connection->close();
+    }
+}
+
+?>
