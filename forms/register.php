68a06176fcc6a03e86beac799363296b61e6c961
Adrian Cuvi <adrian.cuvi@stu.bmcc.cuny.edu>
2020-04-22 20:26:14 -0400

    email encrypt

--- forms/register.php
+++ forms/register.php
@@ -21,7 +21,7 @@ if(mysqli_connect_error()){
 else {
     if($password == $password2){
         $salt = "!@v#$$";
-        $email_encrypt = encrypt($email, "$salt");
+        $email_encrypt = crypt($email, "$salt");
         $password_hash = password_hash($password, PASSWORD_ARGON2I);
         $sql = "INSERT INTO Users(username, email, groupNumber, password)
         values ('$username', '$email_encrypt', '$group', '$password_hash')";
