03495d684515e9fcfd4ddb1f7eef839b58979751
Carlos Bello <cjbh84@gmail.com>
2020-05-06 21:13:48 -0400

    trying to fix this merge

---
+++ php/likert.survey_LOCAL_7105.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if(!isset($_SESSION['token'])) {
+    mysqli_close($conn);
+    session_destroy();
+    header("Location: ../index.php?origin=lig&err=no_credz");
+    exit();
+}
+require_once 'config.php';
+
+    $_SESSION['l1'] = $_POST['l1'];
+    $_SESSION['l2'] = $_POST['l2'];
+    $_SESSION['l3'] = $_POST['l3'];
+    $_SESSION['d1'] = $_POST['d1'];
+    $_SESSION['d2'] = $_POST['d2'];
+    $_SESSION['d3'] = $_POST['d3'];
+
+    mysqli_close($conn);
+    header("Location: ../survey.php");
+    exit();
+?>
