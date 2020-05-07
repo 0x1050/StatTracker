e9f4ede01e0f59e53ae01ad6a5fa62b6a856265f
Carlos Bello <cjbh84@gmail.com>
2020-05-07 16:36:58 -0400

    fix links

--- php/survey.form.likert.php
+++ php/survey.form.likert.php
@@ -39,9 +39,13 @@
     }
     //Write that shit!
     echo "
-        <form id='likert' action='php/likert.survey.php' method='post' enctype='multipart/form-data'>
-        <div class='left'>
-            <h2>$like1</h2>
+        <form id='likert' action='php/likert.survey.php' method='post' enctype='multipart/form-data'>";
+
+    foreach ($cat as $categories) {
+        foreach ($statement as $cat) {
+
+   echo "<div>
+            <h2>$statement</h2>
 
             <div class=\"radios\">
             <input type='radio' name='l1' id='l1' value='$lln' onclick=\"buttonCheck()\"><label>$ll</label><p>
@@ -51,65 +55,13 @@
             <input type='radio' name='l1' id='l5' value='$rrn' onclick=\"buttonCheck()\"><label>$rr</label><p>
             <input type='radio' name='l1' id='l6' class='hidden' value='0' checked>
             </div>
-
-            <h2>$like2</h2>
-
-            <div class=\"radios\">
-            <input type='radio' name='l2' id='l1' value='$lln' onclick=\"buttonCheck()\"><label>$ll</label><p>
-            <input type='radio' name='l2' id='l2' value='$ln' onclick=\"buttonCheck()\"><label>$l</label><p>
-            <input type='radio' name='l2' id='l3' value='$mn' onclick=\"buttonCheck()\"><label>$m</label><p>
-            <input type='radio' name='l2' id='l4' value='$rn' onclick=\"buttonCheck()\"><label>$r</label><p>
-            <input type='radio' name='l2' id='l5' value='$rrn' onclick=\"buttonCheck()\"><label>$rr</label><p>
-            <input type='radio' name='l2' id='l6' class='hidden' value='0' checked>
-            </div>
-
-            <h2>$like3</h2>
-
-            <div class=\"radios\">
-            <input type='radio' name='l3' id='l1' value='$lln' onclick=\"buttonCheck()\"><label>$ll</label><p>
-            <input type='radio' name='l3' id='l2' value='$ln' onclick=\"buttonCheck()\"><label>$l</label><p>
-            <input type='radio' name='l3' id='l3' value='$mn' onclick=\"buttonCheck()\"><label>$m</label><p>
-            <input type='radio' name='l3' id='l4' value='$rn' onclick=\"buttonCheck()\"><label>$r</label><p>
-            <input type='radio' name='l3' id='l5' value='$rrn' onclick=\"buttonCheck()\"><label>$rr</label><p>
-            <input type='radio' name='l3' id='l6' class='hidden' value='0' checked>
-            </div>
         </div>
 
-        <div class='right'>
-            <h2>$dlike1</h2>
+";
+        }
+    }
 
-            <div class=\"radios\">
-            <input type='radio' name='d1' id='r1' value='$lln' onclick=\"buttonCheck()\"><label>$ll</label><p>
-            <input type='radio' name='d1' id='r2' value='$ln' onclick=\"buttonCheck()\"><label>$l</label><p>
-            <input type='radio' name='d1' id='r3' value='$mn' onclick=\"buttonCheck()\"><label>$m</label><p>
-            <input type='radio' name='d1' id='r4' value='$rn' onclick=\"buttonCheck()\"><label>$r</label><p>
-            <input type='radio' name='d1' id='r5' value='$rrn' onclick=\"buttonCheck()\"><label>$rr</label><p>
-            <input type='radio' name='d1' id='r6' class='hidden' value='0' checked>
-            </div>
-
-            <h2>$dlike2</h2>
-
-            <div class=\"radios\">
-            <input type='radio' name='d2' id='r1' value='$lln' onclick=\"buttonCheck()\"><label>$ll</label><p>
-            <input type='radio' name='d2' id='r2' value='$ln' onclick=\"buttonCheck()\"><label>$l</label><p>
-            <input type='radio' name='d2' id='r3' value='$mn' onclick=\"buttonCheck()\"><label>$m</label><p>
-            <input type='radio' name='d2' id='r4' value='$rn' onclick=\"buttonCheck()\"><label>$r</label><p>
-            <input type='radio' name='d2' id='r5' value='$rrn' onclick=\"buttonCheck()\"><label>$rr</label><p>
-            <input type='radio' name='d2' id='r6' class='hidden' value='0' checked>
-            </div>
-
-            <h2>$dlike3</h2>
-
-            <div class=\"radios\">
-            <input type='radio' name='d3' id='r1' value='$lln' onclick=\"buttonCheck()\"><label>$ll</label><p>
-            <input type='radio' name='d3' id='r2' value='$ln' onclick=\"buttonCheck()\"><label>$l</label><p>
-            <input type='radio' name='d3' id='r3' value='$mn' onclick=\"buttonCheck()\"><label>$m</label><p>
-            <input type='radio' name='d3' id='r4' value='$rn' onclick=\"buttonCheck()\"><label>$r</label><p>
-            <input type='radio' name='d3' id='r5' value='$rrn' onclick=\"buttonCheck()\"> <label>$rr</label><p>
-            <input type='radio' name='d3' id='r6' class='hidden' value='0' checked>
-            </div>
-        </div>
-        </form>
+  echo "</form>
         <script src=\"js/likert.js\"></script>
 ";
 ?>
