413af66a7b72061d3189342ba649aff2f7409f55
Carlos Bello <cjbh84@gmail.com>
2020-05-06 21:15:43 -0400

    Finally at the finish line, victory lap is coming

--- php/likert.survey.php
+++ php/likert.survey.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 if(!isset($_SESSION['token'])) {
-<<<<<<< HEAD
     mysqli_close($conn);
     session_destroy();
     header("Location: ../index.php?origin=lig&err=no_credz");
@@ -19,28 +18,4 @@ require_once 'config.php';
     mysqli_close($conn);
     header("Location: ../survey.php");
     exit();
-=======
-	mysqli_close($conn);
-	session_destroy();
-	header("Location: index.php");
-	exit();
-} else {
-	require_once 'config.php';
-
-	if(!isset($_POST['likertliked1']) && !isset($_POST['likertliked2']) {
-		header("Location: ../forms/likert.survey.html");
-	} else {
-		
-		$_SESSION['l1'] = $_POST['likertliked1'];
-		$_SESSION['l2'] = $_POST['likertliked2'];
-		$_SESSION['l3'] = $_POST['likertliked3'];
-		$_SESSION['d1'] = $_POST['likertdisliked1'];
-		$_SESSION['d2'] = $_POST['likertdisliked2'];
-		$_SESSION['d3'] = $_POST['likertdisliked3'];
-
-		header("Location: ../forms/ff.scale.survey.html");
-	}
-}
-
->>>>>>> cf6b01001381de4c97b3d085c885383f0f6421a2
 ?>
