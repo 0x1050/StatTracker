c4534efa61bead90989e2a5b038ad966ea1b402c
M1dn1ghtS0ul69 <62312201+M1dn1ghtS0ul69@users.noreply.github.com>
2020-05-12 09:57:43 -0400

    Create RandomBgColor.js
    Randomly assigns the background color for everyones' background once the project runs.

---
+++ RandomBgColor.js
@@ -0,0 +1,10 @@
+function myFunction()
+{
+  var x = Math.floor(Math.random()*256);
+  var y = Math.floor(Math.random()*256);
+  var z = Math.floor(Math.random()*256);
+  var bgColor = "rgb(" + x + "," + y + "," + z +")";
+  console.log(bgColor);
+  document.body.style.background = bgColor;
+}
+myFunction();
